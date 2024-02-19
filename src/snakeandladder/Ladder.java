28f2b9ebe71f451dac717923a18af44ddaf92d66
Sriganth V <sriganth3@live.com>
2024-02-19 23:38:51 +0530

    Snake and Ladder - Initial Commit

---
+++ src/snakeandladder/Ladder.java
@@ -0,0 +1,28 @@
+package snakeandladder;
+
+public class Ladder {
+
+	private int start;
+	private int end;
+	
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	public Ladder(int start, int end) {
+		super();
+		this.start = start;
+		this.end = end;
+	}
+	
+	
+	
+}
---
+++ src/snakeandladder/Pair.java
@@ -0,0 +1,31 @@
+package snakeandladder;
+
+public class Pair {
+	private int start;
+	
+	private int end;
+	
+	public int getStart() {
+		return start;
+	}
+	
+	public void setStart(int start) {
+		this.start = start;
+	}
+	
+	public int getEnd() {
+		return end;
+	}
+	
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	
+	public Pair(int start, int end) {
+		super();
+		this.start = start;
+		this.end = end;
+	}
+	
+	
+}
